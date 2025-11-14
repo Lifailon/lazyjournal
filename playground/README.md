baa5b35e03cfebc0c47c3cb1ef90426f8e07644d
Lifailon <deadepicboss@gmail.com>
2025-11-14 10:52:32 +0300

    The playground has been updated to demonstrate compose and active logging in containers

--- playground/README.md
+++ playground/README.md
@@ -1,3 +1,3 @@
 # Playground
 
-Demonstration of lazyjournal installation and running on the [Killercoda](https://killercoda.com) playground.
+Demonstration of installing and running the [lazyjournal](https://github.com/Lifailon/lazyjournal?tab=readme-ov-file) binary with pre-launched test containers using Docker Compose and Podman in active logging mode on the [Killercoda](https://killercoda.com/lazyjournal/scenario/playground) playground.
--- playground/foreground.sh
+++ playground/foreground.sh
@@ -1,7 +1,27 @@
-docker run -d --name dozzle-varlog -v /var/log:/logs lifailon/dozzle-varlog:latest
-podman run -d --name dozzle-varlog -v /var/log:/logs lifailon/dozzle-varlog:latest
+# echo -e "\033[32mInstalling k3s (lightweight Kubernetes cluster)...\033[0m"
+# curl -sfL https://get.k3s.io | sh -
 
+# echo -e "\033[32mInstalling Headlamp in the Kubernetes cluster...\033[0m"
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/headlamp/main/kubernetes-headlamp.yaml
+
+echo -e "\033[32mInstalling haproxy for Docker Socket in the Podman...\033[0m"
+podman run -d -p 2375:2375 --name docker-socket-haproxy -v /var/run/docker.sock:/var/run/docker.sock lifailon/docker-socket-haproxy:latest
+while true; do curl -s http://localhost:2375/_ping > /dev/null; sleep 2; done &
+
+echo -e "\033[32mInstalling logporter (lightweight alternative to cadvisor) and Prometheus stack using docker compose...\033[0m"
+curl -sSL https://raw.githubusercontent.com/Lifailon/lazyjournal/refs/heads/main/playground/docker-compose.yml -o docker-compose.yml
+curl -sSL https://raw.githubusercontent.com/Lifailon/lazyjournal/refs/heads/main/playground/prometheus.yml -o prometheus.yml
+docker-compose up -d
+
+echo -e "\033[32mCreate directory and log for custom path in the file system...\033[0m"
+mkdir /test
+curl -sSL https://raw.githubusercontent.com/Lifailon/lazyjournal/refs/heads/main/color.log -o /test/color.log
+
+echo -e "\033[32mInstalling lazyjournal binary from the GitHub repository...\033[0m"
 curl -sS https://raw.githubusercontent.com/Lifailon/lazyjournal/main/install.sh | bash
 . /root/.bashrc
 
-lazyjournal
+lazyjournal \
+    -p /test \
+    -t 5000 \
+    -u 2
